70a248bb7d4d7e300e1c43ad4c047e831d74e3cf
VoidAshfak <1703103@student.ruet.ac.bd>
2024-03-22 23:03:48 +0600

    custom error class and custom api response class

--- src/app.js
+++ src/app.js
@@ -1,3 +1,38 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+
+// CORS Origin Defined
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// To accept json data
+app.use(express.json({
+    limit: "16kb"
+}));
+
+// To accept encoded URL data
+app.use(express.urlencoded({
+    extended: true, // for object inside object
+    limit: "16kb"
+}));
+
+
+app.use(express.static("public")); // use public folder for files
+
+app.use(cookieParser()) // for accessing user's browser cookie
+
+export { 
+    app
+};
+
+
+
 // import express from "express";
 // import cookieParser from "cookie-parser";
 // import cors from "cors";
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv" //using dotenv as early as possible to load the env variables
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 
 //configuring the path (where to pick the environment variables)
@@ -10,3 +11,12 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(`MONGODB CONNECTION FAILED!!`);
+    console.error(error);
+})
