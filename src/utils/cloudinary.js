bb72b0dc645b6516af07e1a6722ff271e58045b5
VoidAshfak <1703103@student.ruet.ac.bd>
2024-03-23 01:47:46 +0600

    Cloudinary file upload and middleware setup for file upload. (Project Setup Finished Around 95%)

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOIDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOIDINARY_API_KEY, 
+  api_secret: process.env.CLOIDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+
+        // UPLOAD FILE ON CLOUDINARY
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        // FILE UPLOAD SUCCESS
+        console.log("File has been successfuly uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //REMOVES THE LOCALLY SAVED TEMP FILE AS THE UPLOAD OPERATION IS FAILED
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
