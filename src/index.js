f9f04abfca98a74446cae4b4fa04dcd5fb4dec5d
VoidAshfak <1703103@student.ruet.ac.bd>
2024-03-25 00:10:21 +0600

    added user register controller

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { app } from "./app.js"
 
 //configuring the path (where to pick the environment variables)
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -61,7 +61,7 @@ userSchema.pre("save", async function (next) {
 
 
     if(!this.isModified("password")) return; //to prevent hashing the password everytime data updates
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
